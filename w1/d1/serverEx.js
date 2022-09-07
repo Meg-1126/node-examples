36cfd249de8b4795e28d18d7a3a9eb25beef7bad
Megumi Akama <kamayatu91@gmail.com>
2022-09-07 16:14:39 -0700

    Add solution for ex objects

--- w1/d1/serverEx.js
+++ w1/d1/serverEx.js
@@ -1,24 +1,40 @@
 const http = require("http");
+const users = { 
+  user1: "Megumi", 
+  user2: "Mauricio", 
+  user3: "Gabriel", 
+  user4: "Yuto", 
+  user5: "Arthur" 
+};
+const products = {
+  item1: "car",
+  item2: "airplane",
+  item3: "bike",
+  item4: "train"
+}
 
 const server = http.createServer((req, res) => {
   console.log("request received", req.url);
 if (req.url === '/cats') {
   res.setHeader("Content-Type", "text/html");
   res.write("<img src='https://pbs.twimg.com/media/Fb-CMgZaAAAHaU2?format=jpg&name=large'>");
-  res.end();
-} else if  (req.url === '/dogs'){
+  // res.end();
+} 
+if  (req.url === '/dogs'){
   res.setHeader("Content-Type", "text/html");
   res.write("<img src='https://pbs.twimg.com/media/FcCwpShaQAA9fYq?format=jpg&name=medium'>");
-  res.end();
-} else if (req.url === '/users') {
-  res.setHeader("Content-Type", "text/html");
-  res.write("");
-  res.end();
-} else if (req.url === '/products') {
-  res.setHeader("Content-Type", "text/html");
-  res.write("");
-  res.end();
+  // res.end();
+} 
+if (req.url === '/users') {
+  res.setHeader("Content-Type", "application/json");
+  res.write(JSON.stringify(users));
+  // res.end();
+} 
+if (req.url === '/products') {
+  res.setHeader("Content-Type", "application/json");
+  res.write(JSON.stringify(products));
 }
+res.end();
 });
 
 server.listen(3000, () => {
